e5f9b889a5c8a8bb7974cffb0a297360e243bbfc
Skice7 <55830756+Skice7@users.noreply.github.com>
2022-03-21 22:16:33 +0100

    commit ej2

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'proyecto-UD35.2.2';
+
+  nombre: string = '';
+  email: any = '';
+  mensaje: string = '';
+  anti_spam: string = '';
+  registros: Array<object> = [];
+
+  guardar() {
+    if (
+      this.nombre != '' &&
+      this.email != '' &&
+      this.mensaje != '' &&
+      this.anti_spam == '5'
+    ) {
+      this.registros.push({
+        nombre: this.nombre,
+        email: this.email,
+        mensaje: this.mensaje,
+      });
+    }
+  }
+}
